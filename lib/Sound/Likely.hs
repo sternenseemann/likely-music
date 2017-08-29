0bb5f55931fea132316b3f9613fb14c508455c31
sternenseemann <git@lukasepple.de>
2017-08-29 20:22:57 +0200

    Add backend JSON parsing of graph scores
    This introduces the soon to be backend of likely music, as well as
    adding an ID to Nodes. Currently the backend only includes code for
    parsing the json graph format to be sent by the frontend (see
    example-graph.json).

--- lib/Sound/Likely.hs
+++ lib/Sound/Likely.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Sound.Likely where
 
 import Data.Maybe
+import Data.Text (Text ())
 import Euterpea
 import System.Random
 import qualified Data.Map as M
@@ -8,8 +10,11 @@ import qualified Data.Set as S
 
 type Probability = Double
 
-data Node = Node { nMusic :: Music Pitch }
-  deriving (Show, Eq, Ord)
+data Node
+  = Node
+  { nId :: Text
+  , nMusic :: Music Pitch
+  } deriving (Show, Eq, Ord)
 
 data Edge
   = Edge
@@ -17,18 +22,19 @@ data Edge
   , eProb :: Probability
   } deriving (Show, Eq, Ord)
 
-type Graph = M.Map Node (S.Set Edge)
+newtype Graph = Graph { unGraph :: M.Map Node (S.Set Edge) }
 
 insertNode :: Node -> Graph -> Graph
-insertNode t = M.insertWith S.union t S.empty
+insertNode t = Graph . M.insertWith S.union t S.empty . unGraph
 
 insertEdge :: Node -> Edge -> Graph -> Graph
-insertEdge n e = insertNode n . M.insertWith S.union n (S.singleton e)
+insertEdge n e =
+  insertNode n . Graph . M.insertWith S.union n (S.singleton e) . unGraph
 
 interpretation :: RandomGen g => g -> Graph -> Node -> Maybe (Music Pitch)
 interpretation gen graph n = (:+:)
   <$> Just (nMusic n)
-  <*> fmap recurse (M.lookup n graph)
+  <*> fmap recurse (M.lookup n (unGraph graph))
   where (prob, gen') = randomR (0.0, 1.0) gen
         recurse edges = if S.null edges
                           then empty
@@ -46,7 +52,7 @@ empty :: Music a
 empty = Prim (Rest 0)
 
 exampleGraph :: Graph
-exampleGraph = M.fromList
-  [ (Node (c 4 qn), S.fromList [ Edge (Node (d 4 qn)) 1 ] )
-  , (Node (d 4 qn), S.fromList [ ])
+exampleGraph = Graph $ M.fromList
+  [ (Node "bla" (c 4 qn), S.fromList [ Edge (Node "blub" (d 4 qn)) 1 ] )
+  , (Node "blub" (d 4 qn), S.fromList [ ])
   ]
